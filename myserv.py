d1d4108c5127f89d47892b73a7eba122310e5e7f
reymond <josephreymond2011@gmail.com>
2020-01-10 15:19:14 +0530

    enclosed inside a function

--- myserv.py
+++ myserv.py
@@ -374,176 +374,177 @@ def seperation(datastring):
                      'lat':lat,'long':long,'speed':speed,'course_status':course_status,
                      'mcc':mcc,'mnc':mnc,'lac':lac,'cell_id':cell_id,'serial_no':serial_no}
 
+
         return datavalue
 
 
-def on_new_client(clientsocket,addr):
- try:
-    while True:
-       # msg = clientsocket.recv(2024)
-       # print('connected by',addr)
-        data = clientsocket.recv(5000)
-        print (sys.stderr, 'received raw "%s"' ,data)
-        print('type raw:',type(data))
-        #unpacked_data = unpacker.unpack(data)
-        #print(sys.stderr, 'unpacked:', unpacked_data)
-        #m = str(msg)
-        #print('message',m)
-        imei = binascii.hexlify(data)
-        imei2 = "null"
-        print('data hexlified - ',imei)
-        #print('type',type(imei))
-        #print('data ascii ed - ',imei2)
-        #print('type',type(imei2))
-        #l = 'LOAD'
-        #p = hex(crc16(imei))
-        str_data = str(imei)
-        #print('01',str_data[8:10])
+def main_func():
+    def on_new_client(clientsocket,addr):
+     try:
+        while True:
+           # msg = clientsocket.recv(2024)
+           # print('connected by',addr)
+            data = clientsocket.recv(5000)
+            print (sys.stderr, 'received raw "%s"' ,data)
+            print('type raw:',type(data))
+            #unpacked_data = unpacker.unpack(data)
+            #print(sys.stderr, 'unpacked:', unpacked_data)
+            #m = str(msg)
+            #print('message',m)
+            imei = binascii.hexlify(data)
+            imei2 = "null"
+            print('data hexlified - ',imei)
+            #print('type',type(imei))
+            #print('data ascii ed - ',imei2)
+            #print('type',type(imei2))
+            #l = 'LOAD'
+            #p = hex(crc16(imei))
+            str_data = str(imei)
+            #print('01',str_data[8:10])
 
-        if str_data[8:10] == '01':              #login packet
-                parsed_data = str_data[26:30]
-                first_data = str_data[2:6]    #7878
-                length = '0501'
-                #print('first-data', first_data)
-                #print('length', length)
-                #print('parsed-data', parsed_data)
-                #p = str(p)
-                #s = p[2:6]
-                error_check =  'ECHE'
-                #print('error_check - ',error_check)
-                strink = first_data+length+parsed_data+error_check+'0d0a'
-                ret1 =  str.encode(strink)
-                #print('pre_output',ret1)
-                mystr = strink[4:12]
-                #print(mystr)
-                ret1 = str.encode(mystr)
-                crc16 = libscrc.x25(binascii.unhexlify(ret1))
-                ra = hex(crc16)
-                #print('pre_errorc', ra)
-                ra = str(ra)
+            if str_data[8:10] == '01':              #login packet
+                    parsed_data = str_data[26:30]
+                    first_data = str_data[2:6]    #7878
+                    length = '0501'
+                    #print('first-data', first_data)
+                    #print('length', length)
+                    #print('parsed-data', parsed_data)
+                    #p = str(p)
+                    #s = p[2:6]
+                    error_check =  'ECHE'
+                    #print('error_check - ',error_check)
+                    strink = first_data+length+parsed_data+error_check+'0d0a'
+                    ret1 =  str.encode(strink)
+                    #print('pre_output',ret1)
+                    mystr = strink[4:12]
+                    #print(mystr)
+                    ret1 = str.encode(mystr)
+                    crc16 = libscrc.x25(binascii.unhexlify(ret1))
+                    ra = hex(crc16)
+                    #print('pre_errorc', ra)
+                    ra = str(ra)
+                    error_check = ra[2:6]
+                    #print('final_error',error_check)
+                    strink = first_data+length+parsed_data+error_check+'0d0a'
+                    print('final_string',strink)
+                    ret2 = str.encode(strink)
+                    print(type(ret2))
+                    print(ret2)
+                    ret1 = binascii.unhexlify(ret2)
+                    #print('crc-',hex(crc16(ret1)))
+                    print('ret - ',ret1)
+                    #print('ret type - ',type(ret1))
+                    clientsocket.send(ret1)
+
+            elif str_data[8:10] == '13':                    #status packet
+                first_data = str_data[2:6]
+                last = '0a0d'
+                #print('first-data',first_data)
+                len = '0513'
+                #print('length',len)
+                serial_no = str_data[18:22]
+                print('serial_no',serial_no)
+                final = len+serial_no
+                re = str.encode(final)
+                crc16 = libscrc.x25(binascii.unhexlify(re))
+                ra = str(hex(crc16))
                 error_check = ra[2:6]
-                #print('final_error',error_check)
-                strink = first_data+length+parsed_data+error_check+'0d0a'
-                print('final_string',strink)
-                ret2 = str.encode(strink)
-                print(type(ret2))
-                print(ret2)
+                #print('errror_check',error_check)
+                finalout = first_data+final+error_check+last
+                #print('final', finalout)
+                ret2 = str.encode(finalout)
+                #print('encode',ret2)
                 ret1 = binascii.unhexlify(ret2)
-                #print('crc-',hex(crc16(ret1)))
-                print('ret - ',ret1)
-                #print('ret type - ',type(ret1))
+                #print('ret - ', ret1)
+                #print('ret type - ', type(ret1))
+                p = str_data[1::]
+                p = p.replace("'", "")
+                #print('after', p)
+                reqbeat = p[8:18]
+                result = status_packet(reqbeat)
+                print("result of status packet",result)
                 clientsocket.send(ret1)
 
-        elif str_data[8:10] == '13':                    #status packet
-            first_data = str_data[2:6]
-            last = '0a0d'
-            #print('first-data',first_data)
-            len = '0513'
-            #print('length',len)
-            serial_no = str_data[18:22]
-            print('serial_no',serial_no)
-            final = len+serial_no
-            re = str.encode(final)
-            crc16 = libscrc.x25(binascii.unhexlify(re))
-            ra = str(hex(crc16))
-            error_check = ra[2:6]
-            #print('errror_check',error_check)
-            finalout = first_data+final+error_check+last
-            #print('final', finalout)
-            ret2 = str.encode(finalout)
-            #print('encode',ret2)
-            ret1 = binascii.unhexlify(ret2)
-            #print('ret - ', ret1)
-            #print('ret type - ', type(ret1))
-            p = str_data[1::]
-            p = p.replace("'", "")
-            #print('after', p)
-            reqbeat = p[8:18]
-            result = status_packet(reqbeat)
-            print("result of status packet",result)
-            clientsocket.send(ret1)
+            elif str_data[8:10] == '12':                #data packet
+                p = str_data[1::]
+                p = p.replace("'", "")
+                # print('after', p)
+                s = p
 
-        elif str_data[8:10] == '12':                #data packet
-            p = str_data[1::]
-            p = p.replace("'", "")
-            # print('after', p)
-            s = p
+                len = s[4:6]
+                protocolno = s[6: 8]
+                date = s[8: 20]
+                satellites = s[20: 22]
+                lat = s[22: 30]
+                long = s[30: 38]
+                speed = s[38: 40]
+                course_status = s[40: 44]
+                mcc = s[44: 48]
+                mnc = s[48: 50]
+                lac = s[50: 54]
+                cell_id = s[54: 60]
+                serial_no = s[60: 64]
 
-            len = s[4:6]
-            protocolno = s[6: 8]
-            date = s[8: 20]
-            satellites = s[20: 22]
-            lat = s[22: 30]
-            long = s[30: 38]
-            speed = s[38: 40]
-            course_status = s[40: 44]
-            mcc = s[44: 48]
-            mnc = s[48: 50]
-            lac = s[50: 54]
-            cell_id = s[54: 60]
-            serial_no = s[60: 64]
-
-            resdate = date_fun(date)
-            latitude = latloncalc(lat)
-            longitude = latloncalc(long)
-            speedans = speedcalc(speed)
-            course = course_status_fun(course_status)
-            mccans = mcc_fun(mcc)
+                resdate = date_fun(date)
+                latitude = latloncalc(lat)
+                longitude = latloncalc(long)
+                speedans = speedcalc(speed)
+                course = course_status_fun(course_status)
+                mccans = mcc_fun(mcc)
 
 
 
-            print('date -',resdate,'lat - ',latitude,'lon - ',longitude,'speed - ',speedans,'course - ',course,'mcc - ',mccans)
+                print('date -',resdate,'lat - ',latitude,'lon - ',longitude,'speed - ',speedans,'course - ',course,'mcc - ',mccans)
 
 
 
 
 
-
-
-            print('len -', len, '__',
-                  'protocolno', protocolno, '__',
-                  'date', date, '__',
-                  'satellites', satellites, '__',
-                  'lat', lat, '__',
-                  'long', long, '__',
-                  'speed', speed, '__',
-                  'course_status', course_status, '__',
-                  'mcc ', mcc, '__',
-                  'mnc ', mnc, '__',
-                  'lac ', lac, '__',
-                  'cell_id ', cell_id, '__',
-                  'serial_no', serial_no, '__')
-
-
-
-        else:
-            print('something new----',str_data)
-            print('closing connection....')
-            clientsocket.close()
- except socket.error as message:
-  print(message)
-  clientsocket.close()
+                print('len -', len, '__',
+                      'protocolno', protocolno, '__',
+                      'date', date, '__',
+                      'satellites', satellites, '__',
+                      'lat', lat, '__',
+                      'long', long, '__',
+                      'speed', speed, '__',
+                      'course_status', course_status, '__',
+                      'mcc ', mcc, '__',
+                      'mnc ', mnc, '__',
+                      'lac ', lac, '__',
+                      'cell_id ', cell_id, '__',
+                      'serial_no', serial_no, '__')
 
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-host = socket.gethostname()  # Get local machine name
-print(host)
-port = 49153                 # Reserve a port for your service.
-
-print('Server started!')
-print(sys.stderr, '\nwaiting for a connection')
-
-s.bind((host, port))        # Bind to the port
-s.listen(5)     # Now wait for client connection.
-unpacker = struct.Struct('I 2s f')
+            else:
+                print('something new----',str_data)
+                print('closing connection....')
+                clientsocket.close()
+     except socket.error as message:
+      print('something wrong is happening',message)
+      clientsocket.close()
 
 
-while True:
-   c, addr = s.accept()     # Establish connection with client.
-   _thread.start_new_thread(on_new_client,(c,addr))
-   print('got connected by ',addr)
-s.close()
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    host = socket.gethostname()  # Get local machine name
+    print(host)
+    port = 49153                 # Reserve a port for your service.
+
+    print('Server started!')
+    print(sys.stderr, '\nwaiting for a connection')
+
+    s.bind((host, port))        # Bind to the port
+    s.listen(5)     # Now wait for client connection.
+    unpacker = struct.Struct('I 2s f')
 
 
+    while True:
+       c, addr = s.accept()     # Establish connection with client.
+       _thread.start_new_thread(on_new_client,(c,addr))
+       print('got connected by ',addr)
+    s.close()
+
+
+main_func()
